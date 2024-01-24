1cd12116559306f16ccb7da7adc9155e23c322ed
ezi-s-u <ljsgoo0617@gmail.com>
2024-01-24 21:48:20 +1100

    :sparkles: [feat] 메인 화면 월급날 API 연결 및 계산 완료

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
+getUserInform()
+
+function getUserInform() {
+  const spanPayday = $('#until-payday');
+  // const spanPayday = document.getElementById('until-payday');
+  console.log(spanPayday);
+
+  const user_id = Cookies.get("user_id");
+  axios.get(`http://localhost:3000/main/${user_id}`)
+    .then((result) => {
+      const startTime = result.data.startTime
+      const endTime = result.data.endTime
+      const payday = new Date(result.data.payday)
+      
+      const todayDate = moment();
+      const paydayDate = moment().year(todayDate.year()).month(todayDate.month()).date(payday.getDate());
+      console.log(paydayDate)
 
 
-location.href = `./index.html?user_id=${id}`;
+      // 만약 현재 날짜가 이미 월급날이 지났다면 다음 달의 월급날로 설정
+      if (todayDate.isAfter(paydayDate)) {
+        paydayDate.add(1, 'months');
+      }
+
+      const untilPayday = paydayDate.diff(todayDate, "days");
+      console.log(untilPayday)
+
+      // ajax 
+      spanPayday.html(untilPayday);
+
+    }).catch((err) => {
+      console.error(err);
+    });
+}
+
+// location.href = `./index.html?user_id=${id}`;
