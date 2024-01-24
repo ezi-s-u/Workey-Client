d7421545409340637b92699e3726926368cfade8
ezi-s-u <ljsgoo0617@gmail.com>
2024-01-24 11:46:34 +1100

    :recycle: [refactor] 로그인 API 수정

--- js/login.js
+++ js/login.js
@@ -9,31 +9,29 @@ function login() {
     "password": pw
   }
 
-  console.log("***" + req.email);
-
-  // axios.post(`http://localhost:3000/users/login`, req)
-  //   .then((result) => {
-  //     Cookies.set("user_id", result.data.id);
-  //     // Cookies.get("user_id")
-  //     location.href = "../index.html";
-  //   }).catch((err) => {
-
-  //   });
-
-  $.support.cors = true;
-  $.ajax({
-    type: 'post',           // 타입 (get, post, put 등등)
-    url: 'http://localhost:3000/users/login',           // 요청할 서버url
-    async: true,            // 비동기화 여부 (default : true)
-    dataType: 'json',       // 데이터 타입 (html, xml, json, text 등등)
-    data: JSON.stringify(req),
-    success: function (result) { // 결과 성공 콜백함수
-      console.log(result);
+  axios.post(`http://localhost:3000/users/login`, req)
+    .then((result) => {
       Cookies.set("user_id", result.data.id);
       // Cookies.get("user_id")
       location.href = "../index.html";
-    }
-  });
+    }).catch((err) => {
+
+    });
+
+  // $.support.cors = true;
+  // $.ajax({
+  //   type: 'post',           // 타입 (get, post, put 등등)
+  //   url: 'http://localhost:3000/users/login',           // 요청할 서버url
+  //   async: true,            // 비동기화 여부 (default : true)
+  //   dataType: 'json',       // 데이터 타입 (html, xml, json, text 등등)
+  //   data: JSON.stringify(req),
+  //   success: function (result) { // 결과 성공 콜백함수
+  //     console.log(result);
+  //     Cookies.set("user_id", result.data.id);
+  //     // Cookies.get("user_id")
+  //     location.href = "../index.html";
+  //   }
+  // });
 }
 
 /* 프론트 */
